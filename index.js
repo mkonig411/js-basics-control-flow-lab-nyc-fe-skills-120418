a2efb3eeb3082e6d571dff1f16f5878d65618b1d
Margot Konig <mkonig411@gmail.com>
2019-01-18 00:40:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function scuberGreetingForFeet(feet) {
 } else if (feet > 2500) {
   return ('No can do.')
 } else if (feet >2000) {
-return ('Costs 30 dollars.')
+return ('I will gladly take your thirty bucks.')
 }
 }
