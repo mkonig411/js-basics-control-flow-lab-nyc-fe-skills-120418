267e6fa49653be5bdf65879b7e47c478174de0c6
Margot Konig <mkonig411@gmail.com>
2019-01-18 00:57:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400 ) {
+  return 'This one is on me!'
+} else if (feet > 2500) {
+  return 'No can do.'
+} else if (feet > 2000) {
+return 'I will gladly take your thirty bucks.'
+}
+}
+
+function ternaryCheckCity(city) {
+  return (city === "NYC" ? "Ok, sounds good." : "No go.")
+} 
+  
+function switchOnCharmFromTip(amount) {
+  switch(amount) {
+    case "generous": {
+      return "Thank you so much."
+      break}
+   case "not as generous":{ 
+      return "Thank you."
+      break}
+     default: {
+      return "Bye."
+      break}
+}}
