f48ad62bcf54226118c8d63d6bd2142f79b6a282
Margot Konig <mkonig411@gmail.com>
2019-01-18 00:38:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ function scuberGreetingForFeet(feet) {
   if (feet <= 400 ) {
   return ('This one is on me!')
 } else if (feet > 2500) {
-  return ('Your ride costs')
-} 
+  return ('No can do.'')
+} else if (feet >2000)
+return ('')
 }
 
