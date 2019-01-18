dd253f74fc39e7e11851d1e84de947a68e5da1bc
Margot Konig <mkonig411@gmail.com>
2019-01-18 00:49:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ return 'I will gladly take your thirty bucks.'
 }
 
 function ternaryCheckCity(city) {
-  return (city === NYC ? "Ok, sounds good." : "No go.")
+  return (city === "NYC" ? "Ok, sounds good." : "No go.")
 
   } 
